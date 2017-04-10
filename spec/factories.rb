bb1b3cd59039caf23d8690ecab66152f0eb71a3c
Sam Landfried <samlandfried@gmail.com>
2017-04-10 16:47:36 -0600

    Test and implement creatures index

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :creature do
-    breed 'bear-turtle'
-    price
-    description
-    image_url
+    sequence :breed do |n|
+      "Breed #{n}"
+    end
+    price '10.00'
   end
 end
--- spec/features/user_can_view_creatures_spec.rb
+++ spec/features/user_can_view_creatures_spec.rb
@@ -4,12 +4,18 @@ RSpec.feature 'User can view creatures' do
   context 'There are creatures' do
     scenario 'User visits /creatures' do
       c1, c2 = create_list(:creature, 2)
+
       visit creatures_path
 
       expect(current_path).to eq '/creatures'
 
-      expect(page).to have_link c1.name
-      expect(page).to have_link c2.name
+      save_and_open_page
+
+      within('ul.creatures') do
+        expect(page).to have_link c1.breed
+        expect(page).to have_link c2.breed
+        expect(page).to have_selector('li', count: 2)
+      end
     end
   end
 
