f0579c05fabf7b443c8a00a1c4876035be18de59
jk1dd <portajonk@gmail.com>
2017-04-10 20:42:07 -0600

    user can remove item from cart

---
+++ spec/features/user_can_remove_item_from_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "User is able to remove items from cart" do
+  context "User is not logged in" do
+    scenario "a user removes an item from their cart" do
+      creature1, creature2 = create_list(:creature, 2)
+
+      visit '/creatures'
+
+      within "li#creature-#{creature1.id}" do
+        click_on "Add to Cart"
+      end
+      within "li#creature-#{creature2.id}" do
+        click_on "Add to Cart"
+      end
+      within "div.nav" do
+        click_on "View Cart"
+      end
+
+      expect(current_path).to eq(carts_path)
+
+      within("li#cart-creature-#{creature1.id}") do
+        click_on "Remove Item"
+      end
+
+      expect(current_path).to eq(carts_path)
+      expect(page).to_not have_content("Breed: #{creature1.breed}")
+      expect(page).to have_content("Breed: #{creature2.breed}")
+    end
+  end
+end
