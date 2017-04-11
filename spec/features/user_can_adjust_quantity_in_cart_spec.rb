5e5a344f087284ebe7743ab6559dd0c3d7bcf16d
mariastenquist <stenquistm@gmail.com>
2017-04-11 15:49:32 -0600

    fixing hound double quotes

--- spec/features/user_can_adjust_quantity_in_cart_spec.rb
+++ spec/features/user_can_adjust_quantity_in_cart_spec.rb
@@ -1,47 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature 'User can adjust quantity' do
-  context "user is not logged in and there is at least one item in the cart" do
+  context 'user is not logged in and there is at least one item in the cart' do
     attr_reader :creature1
 
     before do
       @creature1 = create(:creature)
       creature2 = create(:creature)
       
-      visit "/creatures"
-       within("li:first") do
-        click_on "Add to Cart"
-        click_on "Add to Cart"
+      visit '/creatures'
+       within('li:first') do
+        click_on 'Add to Cart'
+        click_on 'Add to Cart'
       end
-      within("li:last") do
-        click_on "Add to Cart"
+      within('li:last') do
+        click_on 'Add to Cart'
       end
-      visit "/carts"
+      visit '/carts'
     end
 
-    scenario "user increases cart quantity" do
+    scenario 'user increases cart quantity' do
       within("li#cart-creature-#{creature1.id}") do
         expect(page).to have_content('Quantity: 2')
-        click_on "Increase Quantity" 
-        expect(current_path).to eq "/carts"
+        click_on 'Increase Quantity' 
+        expect(current_path).to eq '/carts'
         expect(page).to have_content('Quantity: 3')
         expect(page).to have_content('Price: 30.00')
       end
-      within("div.cart-total") do
-        expect(page).to have_content("Total Price: 40.00")
+      within('div.cart-total') do
+        expect(page).to have_content('Total Price: 40.00')
       end
     end
-    scenario "user decreases cart quantity" do
+    scenario 'user decreases cart quantity' do
 
       within("li#cart-creature-#{creature1.id}") do
         expect(page).to have_content('Quantity: 2')
-        click_on "Decrease Quantity" 
-        expect(current_path).to eq "/carts"
+        click_on 'Decrease Quantity' 
+        expect(current_path).to eq '/carts'
         expect(page).to have_content('Quantity: 1')
         expect(page).to have_content('Price: 10.00')
       end
-      within("div.cart-total") do
-        expect(page).to have_content("Total Price: 20.00")
+      within('div.cart-total') do
+        expect(page).to have_content('Total Price: 20.00')
       end
     end
   end
