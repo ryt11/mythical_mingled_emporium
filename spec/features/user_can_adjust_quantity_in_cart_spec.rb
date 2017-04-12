f30ba5338d2e7a30366e756eca5916d0399f868c
Sam Landfried <samlandfried@gmail.com>
2017-04-11 22:38:22 -0600

    Update #total_price for Cart to reflect quantity
    Test still breaks because of the Capybara confusion b/t
    page.text and page.body.
    Add number_to_currency view helper
    Update test html for div.nav-wrapper
    Update test formatting for money
    Fix some Hound complaints

--- spec/features/user_can_adjust_quantity_in_cart_spec.rb
+++ spec/features/user_can_adjust_quantity_in_cart_spec.rb
@@ -7,13 +7,14 @@ RSpec.feature 'User can adjust quantity' do
     before do
       @creature1 = create(:creature)
       creature2 = create(:creature)
-      
+
       visit '/creatures'
-       within('li:first') do
+
+      within("#creature-#{@creature1.id}") do
         click_on 'Add to Cart'
         click_on 'Add to Cart'
       end
-      within('li:last') do
+      within("#creature-#{creature2.id}") do
         click_on 'Add to Cart'
       end
       visit '/carts'
@@ -22,26 +23,26 @@ RSpec.feature 'User can adjust quantity' do
     scenario 'user increases cart quantity' do
       within("li#cart-creature-#{creature1.id}") do
         expect(page).to have_content('Quantity: 2')
-        click_on 'Increase Quantity' 
+        click_on 'Increase Quantity'
         expect(current_path).to eq '/carts'
         expect(page).to have_content('Quantity: 3')
-        expect(page).to have_content('Price: 30.00')
+        expect(page).to have_content('Price: $30.00')
       end
       within('div.cart-total') do
-        expect(page).to have_content('Total Price: 40.00')
+        expect(page).to have_content('Total Price: $40.00')
       end
     end
-    scenario 'user decreases cart quantity' do
 
+    scenario 'user decreases cart quantity' do
       within("li#cart-creature-#{creature1.id}") do
         expect(page).to have_content('Quantity: 2')
-        click_on 'Decrease Quantity' 
+        click_on 'Decrease Quantity'
         expect(current_path).to eq '/carts'
         expect(page).to have_content('Quantity: 1')
-        expect(page).to have_content('Price: 10.00')
+        expect(page).to have_content('Price: $10.00')
       end
       within('div.cart-total') do
-        expect(page).to have_content('Total Price: 20.00')
+        expect(page).to have_content('Total Price: $20.00')
       end
     end
   end
