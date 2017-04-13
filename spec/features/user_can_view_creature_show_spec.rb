a2bfe34bab45ac98c782afadb05226f18ca6586c
dannyradden <dradden@gmail.com>
2017-04-13 14:50:35 -0600

    write creature show page test

--- spec/features/user_can_view_creature_show_spec.rb
+++ spec/features/user_can_view_creature_show_spec.rb
@@ -4,11 +4,10 @@ RSpec.feature 'User can view a creature show page' do
   context 'Where a creature is retired' do
     scenario 'From a past order page' do
       creature = create(:creature)
-      creature.retired!
       user = create(:user)
       user.orders << create(:order)
       order = user.orders.first
-      order.creatures << (creature(quantity: 1))
+      OrderCreature.create(order: order, creature: creature, quantity: 2)
 
       visit order_path(order)
 
@@ -17,7 +16,26 @@ RSpec.feature 'User can view a creature show page' do
       expect(current_path).to eq creature_path(creature)
 
       expect(page).to_not have_button 'Add to Cart'
-      axpect(page).to have_content 'Item Retired'
+      expect(page).to have_content 'Item Retired'
+    end
+  end
+
+  context 'Where a creature is active' do
+    scenario 'From the creatures page' do
+      creature = create(:creature)
+
+      visit creatures_path
+
+      click_on creature.breed
+
+      expect(current_path).to eq creature_path(creature)
+
+      expect(page).to have_css("img[src*='#{creature.id}?set']")
+      expect(page).to have_content "Breed: #{creature.breed}"
+      expect(page).to have_content "Price: $#{creature.price}0"
+      expect(page).to have_content "Description: #{creature.description}"
+
+      expect(page).to have_button 'Add to Cart'
     end
   end
 end
