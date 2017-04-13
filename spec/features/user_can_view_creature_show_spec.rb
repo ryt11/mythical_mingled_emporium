d6b5fc266bb61d1c5a735512e9170a8cddbb6437
Sam Landfried <samlandfried@gmail.com>
2017-04-13 14:29:17 -0600

    Test creature enums for status

--- spec/features/user_can_view_creature_show_spec.rb
+++ spec/features/user_can_view_creature_show_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
-RSpec.feature 'User can view a creature show page' do 
+RSpec.feature 'User can view a creature show page' do
   context 'Where a creature is retired' do
     scenario 'From a past order page' do
-      # As a user if I visit an item page and that item has been retired
-      creature1 = create(:creature)
-      # creature1.retired!
-      user1 = create(:user)
-      order1 = create(:order)
-      byebug
+      creature = create(:creature)
+      creature.retired!
+      user = create(:user)
+      user.orders << create(:order)
+      order = user.orders.first
+      order.creatures << (creature(quantity: 1))
 
-      # visit 
-      # Then I should still be able to access the item page
-      
-      # And I should not be able to add the item to their cart
-      # And I should see in place of the "Add to Cart" button or link - "Item Retired"
+      visit order_path(order)
 
+      click_on creature.breed
+
+      expect(current_path).to eq creature_path(creature)
+
+      expect(page).to_not have_button 'Add to Cart'
+      axpect(page).to have_content 'Item Retired'
     end
   end
 end
