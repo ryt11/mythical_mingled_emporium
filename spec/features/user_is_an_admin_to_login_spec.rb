af7521a0dcf98011d948457194cbbbf2b022f8d7
dannyradden <dradden@gmail.com>
2017-04-14 17:47:04 -0600

    Add tests for a registered user
    Remove file accidently created

--- spec/features/user_is_an_admin_to_login_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'user can login as an admin'do 
-
-  context 'user is an admin'
--- spec/features/user_permissions_spec.rb
+++ spec/features/user_permissions_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.feature 'User is not able to see other users information' do 
-  context 'a user is logged in' do 
-    before do 
+RSpec.feature 'User Permissions' do
+  context 'a user is logged in' do
+    before do
       user1, user2 = create_list(:user, 2)
       @order1, @order2, @order3, @order4 = create_list(:order, 4)
       user1.orders << [@order1, @order2]
       user2.orders << [@order3, @order4]
     end
-    scenario 'a user can only see their information' do
 
+    scenario 'a user can only see their information' do
       visit orders_path
 
       within('table.orders') do
@@ -22,10 +22,11 @@ RSpec.feature 'User is not able to see other users information' do
       end
     end
 
-    visit admin_dashboard_path
+    scenario 'a user cannot view admin dashboard' do
+      visit admin_dashboard_path
 
-    expect(user.admin?).to eq(:false) #check simple data type
-    expect(current_path).to_not 
-
+      expect(response).to have_http_status(404)
+      expect(page).to_not have_content('Admin Dashboard')
+    end
   end
 end
