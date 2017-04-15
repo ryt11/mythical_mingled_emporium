d498f6e52b6f09b252cf19ea88c1945f389ae10a
jk1dd <portajonk@gmail.com>
2017-04-15 13:56:03 -0600

    Direct admin to admin dashboard upon login
    hound fixes

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,10 +3,6 @@ class Admin::BaseController < ApplicationController
 
   private
 
-  def current_admin?
-    current_user && current_user.admin?
-  end
-
   def check_admin
     render file: 'public/404', status: 404 unless current_admin?
   end
