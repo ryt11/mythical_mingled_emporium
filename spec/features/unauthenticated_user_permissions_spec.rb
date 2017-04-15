810be47d4422f420ef55a591d2fd0db818bdd5e9
Sam Landfried <samlandfried@gmail.com>
2017-04-15 12:30:11 -0600

    Restrict visitor access
    Add feature test for admin dashboard
    hound issues

--- spec/features/unauthenticated_user_permissions_spec.rb
+++ spec/features/unauthenticated_user_permissions_spec.rb
@@ -8,30 +8,36 @@ RSpec.feature 'Unauthenticated user permissions' do
       user1.orders << [@order1, @order2]
     end
 
-    scenario 'a user can only see their information' do
+    scenario 'so she cannot access orders' do
       visit orders_path
 
-      within('table.orders') do
-        expect(page).to_not have_content(@order1.id)
-        expect(page).to_not have_content(@order2.id)
-        expect(page).to_not have_content(@order3.id)
+      expect(current_path).to eq login_path
+      within 'div.flash' do
+        expect(page).to have_content 'Oops! You must be logged in to do that.'
+      end
+    end
+
+    scenario 'so she cannot see a dashboard' do
+      visit dashboard_path
+
+      expect(current_path).to eq login_path
+      within 'div.flash' do
+        expect(page).to have_content 'Oops! You must be logged in to do that.'
       end
     end
 
     scenario 'a user cannot view admin dashboard' do
       visit admin_dashboard_path
 
-      expect(response).to have_http_status(404)
+      expect(page).to have_http_status(404)
       expect(page).to_not have_content('Admin Dashboard')
     end
 
     scenario 'will be redirected to login/create account when they checkout' do
       visit cart_path
 
-      click_on 'Checkout' # We will have to change the previous test that makes
-                          # it so that it doesnt exists
-      expect(current_path).to eq(login_path)
-      expect(page).to have_content('Login or create an account to checkout')
+      expect(page).to_not have_button 'Checkout'
+      expect(page).to have_content('Login or Create Account to Checkout')
     end
   end
 end
