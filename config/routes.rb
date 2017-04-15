a04aa10f4c1d4ade3037aba20d5758666ed480e8
mariastenquist <stenquistm@gmail.com>
2017-04-15 11:02:40 -0600

    limit users from seeing others orders

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
 
   get '/:category', to: 'categories#show', as: 'category'
 end
--- spec/features/user_cannot_see_others_data_spec.rb
+++ spec/features/user_cannot_see_others_data_spec.rb
@@ -4,28 +4,30 @@ RSpec.feature 'User is not able to see other users information' do
   context 'a user is logged in' do 
     before do 
       user1, user2 = create_list(:user, 2)
-      @order1, @order2, @order3, @order4 = create_list(:order, 4)
+      @order1, @order2, @order3, @order4 = create_list(:order_with_diff_totals, 4)
       user1.orders << [@order1, @order2]
       user2.orders << [@order3, @order4]
-    end
-    scenario 'a user can only see their information' do
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    end
+
+    scenario 'a user can only see their information' do
       visit orders_path
 
       within('table.orders') do
         expect(page).to have_content(@order1.id)
         expect(page).to have_content(@order1.total)
       end
+
       within('table.orders') do
-        expect(page).to_not have_content(@order3.id)
+        expect(page).to_not have_link("Order #: #{@order3.id}")
         expect(page).to_not have_content(@order3.total)
       end
+
+      visit admin_dashboard_path
+
+      expect(user.admin?).to eq(:false) #check simple data type
+      expect(current_path).to_not 
     end
-
-    visit admin_dashboard_path
-
-    expect(user.admin?).to eq(:false) #check simple data type
-    expect(current_path).to_not 
-
   end
 end
