0409703af910aefdbc058f9c6c9716ce9635ba0c
Sam Landfried <samlandfried@gmail.com>
2017-04-15 15:56:25 -0600

    Add creature_card view helper

--- app/helpers/creatures_helper.rb
+++ app/helpers/creatures_helper.rb
@@ -1,2 +1,20 @@
 module CreaturesHelper
+  def creature_card(creature)
+    %Q*
+    <div class="col l3 m4 s6 xs12">
+      <div id="creature-#{creature.id}" class="card small">
+        <div class="card-image">
+           #{image_tag creature.image_url}
+        </div>
+        <div class="card-content">
+          Breed: #{ link_to creature.breed, creature_path(creature) }<br>
+          Price: #{ creature.price }<br>
+        </div>
+        <div class="cart-action">
+          #{ button_to "Add to Cart", cart_path(creature_id: creature.id) }
+        </div>
+      </div>
+    </div>
+    *.html_safe
+  end
 end
