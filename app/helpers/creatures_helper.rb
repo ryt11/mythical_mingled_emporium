a5f75c0092dd4d0630b1676a1110b5c1e6c60b33
Sam Landfried <samlandfried@gmail.com>
2017-04-16 13:50:48 -0600

    Style cards

--- app/helpers/creatures_helper.rb
+++ app/helpers/creatures_helper.rb
@@ -1,11 +1,8 @@
 module CreaturesHelper
   def creature_card(creature)
     %Q*
-    <div class="col l3 m4 s6 xs12">
-      <div id="creature-#{creature.id}" class="card small">
-        <div class="card-image" style="background-image: url('#{creature.image_url}')">
-
-        </div>
+    <div class="col xl3 l4 m6 s12">
+      <div id="creature-#{creature.id}" class="card small creature-card">
         <div class="card-content">
           Breed: #{ link_to creature.breed, creature_path(creature) }<br>
           Price: #{ creature.price }<br>
