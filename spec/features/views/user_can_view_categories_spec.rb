a33531ca0c1f4659127dd4b157c22abeee190eb5
Sam Landfried <samlandfried@gmail.com>
2017-04-16 23:35:23 -0600

    Test categories index page contains creatures

--- spec/features/views/user_can_view_categories_spec.rb
+++ spec/features/views/user_can_view_categories_spec.rb
@@ -1,29 +1,55 @@
 require 'rails_helper'
 
 RSpec.feature 'User can view categories' do
-  context 'There are categories' do
-    scenario 'User visits /categories' do
-      c1, c2 = create_list(:category, 2)
+  context 'There are categories and creatures' do
+    attr_reader :category_1, :category_2, :creatures
+    before do
+      @category_1, @category_2 = create_list(:category, 2)
+      @creatures = create_list(:creature, 3)
+      category_1.creatures << creatures[0, 1]
+      category_2.creatures << creatures.last
+    end
 
-      visit categories_path
+    scenario 'User visits /categories' do
+
+      visit root_path
+
+      click_on 'Shop'
 
       expect(current_path).to eq '/categories'
 
-      within('ul.categories') do
-        expect(page).to have_link c1.name
-        expect(page).to have_link c2.name
-        expect(page).to have_selector('li', count: 2)
+      within "#category-#{category_1.id}" do
+        within '.creature-card:first' do
+          expect(page).to have_selector "#creature-#{creatures.first.id}"
+        end
+
+        within '.creature-card:last' do
+          expect(page).to have_selector "#creature-#{creatures[1].id}"
+        end
+
+        expect(page).to_not have_selector "#creature-#{creatures.last.id}"
       end
+
+      within "#category-#{category_2.id}" do
+        within '.creature-card' do
+          expect(page).to have_selector "#creature-#{creatures.last.id}"
+        end
+
+        expect(page).to_not have_selector "#creature-#{creatures.first.id}"
+        expect(page).to_not have_selector "#creature-#{creatures[1].id}"
+      end
+
+      with_tag('.creature-card', count: 1)
     end
   end
+end
 
-  context 'There are no categories' do
-    scenario 'User visits /categories' do
-      visit categories_path
+context 'There are no categories' do
+  scenario 'User visits /categories' do
+    visit categories_path
 
-      within('ul.categories') do
-        expect(page).to have_selector('li', count: 0)
-      end
+    within('.categories') do
+      expect(page).to have_link count: 0
     end
   end
 end
