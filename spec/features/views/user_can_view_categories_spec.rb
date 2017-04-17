1cc233024c39b354a609910805b3387d2d1333dc
Sam Landfried <samlandfried@gmail.com>
2017-04-16 23:41:34 -0600

    hound issues

--- spec/features/views/user_can_view_categories_spec.rb
+++ spec/features/views/user_can_view_categories_spec.rb
@@ -2,23 +2,22 @@ require 'rails_helper'
 
 RSpec.feature 'User can view categories' do
   context 'There are categories and creatures' do
-    attr_reader :category_1, :category_2, :creatures
+    attr_reader :category1, :category2, :creatures
     before do
-      @category_1, @category_2 = create_list(:category, 2)
+      @category1, @category2 = create_list(:category, 2)
       @creatures = create_list(:creature, 3)
-      category_1.creatures << creatures[0, 1]
-      category_2.creatures << creatures.last
+      category1.creatures << creatures[0, 1]
+      category2.creatures << creatures.last
     end
 
     scenario 'User visits /categories' do
-
       visit root_path
 
       click_on 'Shop'
 
       expect(current_path).to eq '/categories'
 
-      within "#category-#{category_1.id}" do
+      within "#category-#{category1.id}" do
         within '.creature-card:first' do
           expect(page).to have_selector "#creature-#{creatures.first.id}"
         end
@@ -30,7 +29,7 @@ RSpec.feature 'User can view categories' do
         expect(page).to_not have_selector "#creature-#{creatures.last.id}"
       end
 
-      within "#category-#{category_2.id}" do
+      within "#category-#{category2.id}" do
         within '.creature-card' do
           expect(page).to have_selector "#creature-#{creatures.last.id}"
         end
